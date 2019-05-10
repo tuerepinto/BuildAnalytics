9c2139dd1102039b7d5a25c119b6e10fac876b75
Tuerê Pinto <tuerepinto@gmail.com>
2019-05-10 11:10:50 -0300

    Update buildAuto.ps1

--- buildAuto.ps1
+++ buildAuto.ps1
@@ -1,13 +1,26 @@
-$Dir = get-childItem C:\Localiza -Recurse  
-$List = $Dir | Where-Object {$_.extension -eq ".sln"} 
-$List |Format-Table fullname |out-file C:\Log\sln.txt
+$Dir = get-childItem C:\Localiza -Recurse;  
 
-$text = Get-Content -Path C:\log\sln.txt
+$List = $Dir | Where-Object {$_.Extension -eq ".sln"};
+$List |Format-Table fullname |out-file C:\Log\sln.txt;
 
-foreach($item in $text)
+$Log = $Dir | Where-Object {$_.Extension -eq ".log"};
+$Log |Format-Table fullname |Out-File C:\Log\log.txt;
+
+$Slntext = Get-Content -Path C:\log\sln.txt;
+$LogText =  Get-Content -Path C:\log\log.txt;
+
+$NewFolder = 'C:\Log\Export\';
+
+foreach($item in $Slntext)
 {
     $NameSln = $item.replace('.sln','.log');  
-    dotnet clean $item; dotnet clean --configuration $item; 
-    dotnet restore $item; dotnet build /flp:v=diag /flp:logfile=$NameSln.log $item 
+    dotnet clean $item; 
+    dotnet restore $item; dotnet build /flp:v=diag /flp:logfile=$NameSln $item;
 }
 
+foreach ($item in $LogText)
+{ 
+    $item.Replace(' .log', '');
+    $item.Replace(' ', '');
+    Move-Item -Path $item -Destination $NewFolder;
+}
